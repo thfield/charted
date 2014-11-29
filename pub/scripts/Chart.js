914b5205969f95df79a2113e5f0e0a985e426f01
thfield <thfield@gmail.com>
2014-11-29 12:13:31 -0800

    fix to make npm test pass

--- pub/scripts/Chart.js
+++ pub/scripts/Chart.js
@@ -507,9 +507,9 @@ Chart.prototype.yAxisLabelHTML = function (interval) {
   return _.template('<div class="y-axis-label" style="top:<%- top %>px"><%- display %></div>', interval)
 }
 
-Chart.prototype.drawPie = function (index) {
+Chart.prototype.drawPie = function () {
   //todo: pie doesn't attach to correct series when chart gets moved
-  var dataarray = []
+  var dataarray = [],
       seriesesLength = this.data.getSeriesCount();
 
   for (var i=0; i < seriesesLength; i++){
@@ -533,19 +533,19 @@ Chart.prototype.drawPie = function (index) {
       .sort(null)
       .value(function(d) { return d; });
 
-  var svg = d3.select('.selection-pie').append("svg")
-      .attr("width", width)
-      .attr("height", height)
-    .append("g")
-      .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+  var svg = d3.select('.selection-pie').append('svg')
+      .attr('width', width)
+      .attr('height', height)
+    .append('g')
+      .attr('transform', 'translate(' + width / 2 + ',' + height / 2 + ')');
 
-  var g = svg.selectAll(".arc")
+  var g = svg.selectAll('.arc')
       .data(pie(dataarray))
-    .enter().append("g")
-      .attr("class", "arc");
+    .enter().append('g')
+      .attr('class', 'arc');
 
-  g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d,i) { return color(i); });
+  g.append('path')
+      .attr('d', arc)
+      .style('fill', function(d,i) { return color(i); });
 }
 
